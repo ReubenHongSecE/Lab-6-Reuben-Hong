76fc995154869c800d3a7c5543b3206bc801804e
ReubenHongSecE <n01532527@humber.ca>
2024-10-08 09:03:46 -0400

    Update lab6task2.js

--- lab6task2.js
+++ lab6task2.js
@@ -1,14 +1,14 @@
 //Task 2: Exploring Loops
 //1.
-for (let i = 0; i < 11; i++) {
-    console.log(i);
+for (let i = 0; i < 11; i++) { //for loop that repeats iterates from 0 to 10
+    console.log(i); //prints the value of i for the current iteration
 } 
 
 //2.
-console.log("2. First five multiples of 3:")
-for (let x = 1; x < 6; x++) {
+console.log("2. First five multiples of 3:") 
+for (let x = 1; x < 6; x++) { //for loop that interates from 1 to 5
     let y = 3;
 
-    console.log(x + ")", x*y)
+    console.log(x + ")", x*y) //prints the the product of the value of y and the value of x of the current iteration
 }
 
