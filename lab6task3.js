cfd7b607763fb4564fdca42691e1c461e42b4cfc
ReubenHongSecE <n01532527@humber.ca>
2024-10-08 09:07:45 -0400

    Update lab6task3.js

--- lab6task3.js
+++ lab6task3.js
@@ -1,14 +1,14 @@
 //Task 3: Discovering Functions
 //1. 
-function addNumbers(num1, num2) {
-    return num1 + num2; //2.
+function addNumbers(num1, num2) { //declaring the function addNumbers and setting two variables as its paramters
+    return num1 + num2; //2. returns the sum of both variables
 }
 //3.
-console.log(addNumbers(5,6));
+console.log(addNumbers(5,6)); //calls the function, executes it and outputs based on the updated set of parameters
 
 //4.
-function multiplyNumbers(num1, num2) {
-    return num1 * num2; 
+function multiplyNumbers(num1, num2) { //declaring the function multiplyNumbers and setting two variables as its paramters
+    return num1 * num2; //returns the product of both variables
 }
 
-console.log(multiplyNumbers(3,4));
+console.log(multiplyNumbers(3,4)); //calls the function, executes it and outputs based on the updated parameters
