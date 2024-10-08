2c0c2a5507971276a4abe9ac461be9b11586c0a8
ReubenHongSecE <n01532527@humber.ca>
2024-10-08 08:53:20 -0400

    Update lab6task2.js

---
+++ lab6task2.js
@@ -0,0 +1,14 @@
+//Task 2: Exploring Loops
+//1.
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+} 
+
+//2.
+console.log("2. First five multiples of 3:")
+for (let x = 1; x < 6; x++) {
+    let y = 3;
+
+    console.log(x + ")", x*y)
+}
+
