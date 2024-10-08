873e58bdf172ec0ea1194cb448c73ed052b7d09c
ReubenHongSecE <n01532527@humber.ca>
2024-10-08 08:58:31 -0400

    Update lab6task3.js

---
+++ lab6task3.js
@@ -0,0 +1,14 @@
+//Task 3: Discovering Functions
+//1. 
+function addNumbers(num1, num2) {
+    return num1 + num2; //2.
+}
+//3.
+console.log(addNumbers(5,6));
+
+//4.
+function multiplyNumbers(num1, num2) {
+    return num1 * num2; 
+}
+
+console.log(multiplyNumbers(3,4));
